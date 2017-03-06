f6ed8d14faaa9c9c3db1adab55d4f11a3d2d12c5
cshs20120831 <cshs20120831@gmail.com>
2017-03-07 01:35:30 +0800

    由左至右閃燈

---
+++ _1.ino
@@ -0,0 +1,47 @@
+const byte startPin = 1;
+const byte endPin = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  for (byte i = startPin; i<= endPin; i++) 
+  {
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop() 
+{
+   byte i;
+  byte lightPin = startPin;
+     for (i=startPin; i<= endPin; i++)
+  {
+    digitalWrite(i,LOW);
+    
+  	if (lightPin < endPin) 
+  	{
+    	lightPin ++;
+  	} 
+  	else 
+  	{
+    	lightPin = endPin;
+  	}
+  	digitalWrite(lightPin, HIGH);
+  	delay (100);
+  } 
+  lightPin = endPin;
+  for (i=endPin; i>= startPin; i--)
+  {
+    digitalWrite(i,LOW);
+  	if (lightPin > startPin) 
+ 	{
+    	lightPin --;
+  	} 
+  	else 
+  	{
+    	lightPin = startPin;
+  	}
+  	digitalWrite(lightPin, HIGH);
+  	delay (100);
+  } 
+}
+
